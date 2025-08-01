c23a3bdcba0c1c82415df45d4318c14833e82cb7
Artur Knak <arturknak@protonmail.com>
2025-08-01 17:41:07 -0300

    Creating main loop. Connecting Menu and Game classes.

--- code/game.py
+++ code/game.py
@@ -1,9 +1,26 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame
+from code.menu import Menu
+
 
 class Game:
     def __init__(self):
-        self.window = None
+        # pygame.init é obrigatório para iniciar o pygame e todas
+        # as suas dependências!
+        pygame.init()
+        # Criando a janela do jogo.
+        self.window = pygame.display.set_mode(size=(600, 480))
 
     def run(self, ):
-        pass
+        # A janela precisa de um loop para se manter aberta.
+        while True:
+            menu = Menu(self.window)
+            menu.run()
+
+            # Check for all events.
+            # Criando evento para fechar janela!
+            # for event in pygame.event.get():
+            #     if event.type == pygame.QUIT:
+            #         pygame.quit()  # Close window
+            #         quit()  # End pygame
--- main.py
+++ main.py
@@ -1,19 +1,4 @@
-import pygame
+from code.game import Game
 
-print('Setup start.')
-# pygame.init é obrigatório para iniciar o pygame e todas
-# as suas dependências!
-pygame.init()
-# Criando a janela do jogo.
-window = pygame.display.set_mode(size=(600, 480))
-print('Setup end.')
-
-print('Loop start.')
-# A janela precisa de um loop para se manter aberta.
-while True:
-    # Check for all events.
-    # Criando evento para fechar janela!
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit() # Close window
-            quit()        # End pygame
+game = Game()
+game.run()

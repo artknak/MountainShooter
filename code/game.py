80f5a095fec4e3dfaada73c67b17e358f9d8627c
Artur Knak <arturknak@protonmail.com>
2025-08-29 11:37:26 -0300

    SQLite3 implementation for score using proxy design pattern

--- code/game.py
+++ code/game.py
@@ -5,6 +5,7 @@ import pygame
 from code.const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
 from code.level import Level
 from code.menu import Menu
+from code.score import Score
 
 
 class Game:
@@ -18,6 +19,7 @@ class Game:
     def run(self):
         # A janela precisa de um loop para se manter aberta.
         while True:
+            score = Score(self.window)
             menu = Menu(self.window)
             menu_return = menu.run()  # Recebe o return (quando é registrado ENTER) do menu.py
 
@@ -30,10 +32,16 @@ class Game:
                 if level_return:
                     level = Level(self.window, 'Level2', menu_return, player_score)
                     level_return = level.run(player_score)
+                    if level_return:
+                        score.save(menu_return, player_score)
+
+            elif menu_return == MENU_OPTION[3]:
+                score.show()
 
             elif menu_return == MENU_OPTION[4]:  # Opção EXIT (fecha o jogo)
                 pygame.quit()
                 quit()
+
             else:
                 pass
 
